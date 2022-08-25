c08cbfba479f4622abd9fe14fe91cf568d96e501
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-25 05:03:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+var dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "red";
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 8);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 6}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace("px", "");
+  var left = parseInt(leftNumbers, 8);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 6}px`;
+  }
+}
